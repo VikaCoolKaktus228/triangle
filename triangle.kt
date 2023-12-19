497ce19a756ac5f9cc705ccb533ce7bc0c06cea6
Gribabas <144676939+VikaCoolKaktus228@users.noreply.github.com>
2023-12-19 13:02:38 +0300

    Update triangle.kt

--- triangle.kt
+++ triangle.kt
@@ -1,49 +1,52 @@
-import java.lang.IllegalArgumentException
-
-// ф-ция чтобы вычислить плозадь треугольника
-fun calculateTriangleArea(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double): Double {
-    return (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
-}
-
-// ерить находится ли точка в теуглольнике фунция чтобы пров
-fun isPointInsideTriangle(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x: Double, y: Double): Boolean {
-    val triangleArea = calculateTriangleArea(x1, y1, x2, y2, x3, y3)
-    val area1 = calculateTriangleArea(x, y, x2, y2, x3, y3)
-    val area2 = calculateTriangleArea(x1, y1, x, y, x3, y3)
-    val area3 = calculateTriangleArea(x1, y1, x2, y2, x, y)
-
-    return triangleArea == area1 + area2 + area3
-}
-
 fun main() {
-    try {
-        println("введите координаты вершин треугольника:")
-        print("вершина 1 (x1 y1): ")
-        val x1 = readLine()!!.toDouble()
-        val y1 = readLine()!!.toDouble()
+    val triangle = inputTriangle()
+    val point = inputPoint()
 
-        print("вершина 2 (x2 y2): ")
-        val x2 = readLine()!!.toDouble()
-        val y2 = readLine()!!.toDouble()
-
-        print("вершина 3 (x3 y3): ")
-        val x3 = readLine()!!.toDouble()
-        val y3 = readLine()!!.toDouble()
-
-        println("введите координаты точки:")
-        print("точка (x y): ")
-        val x = readLine()!!.toDouble()
-        val y = readLine()!!.toDouble()
-
-        val isInside = isPointInsideTriangle(x1, y1, x2, y2, x3, y3, x, y)
-        if (isInside) {
-            println("точка внутри треугольника")
-        } else {
-            println("точка вне труегольника")
-        }
-    } catch (e: IllegalArgumentException) {
-        println("введите цифры пожалуйста")
-    } catch (e: NullPointerException) {
-        println("введите новые координаты")
+    if (isInsideTriangle(triangle, point)) {
+        println("Точка находится внутри треугольника")
+    } else {
+        println("Точка находится вне треугольника")
     }
+}
+
+data class Point(val x: Double, val y: Double)
+data class Triangle(val A: Point, val B: Point, val C: Point)
+
+fun inputTriangle(): Triangle {
+    println("Введите координаты вершин треугольника:")
+    val A = inputPoint("A")
+    val B = inputPoint("B")
+    val C = inputPoint("C")
+    return Triangle(A, B, C)
+}
+
+fun inputPoint(name: String = ""): Point {
+    var x: Double
+    var y: Double
+    while (true) {
+        try {
+            print("Введите координаты точки $name (x y): ")
+            val input = readLine()!!.split(" ")
+            require(input.size == 2) { "Введите две координаты через пробел" }
+            x = input[0].toDouble()
+            y = input[1].toDouble()
+            break
+        } catch (e: Exception) {
+            println("Ошибка: ${e.message}. Попробуйте еще раз.")
+        }
+    }
+    return Point(x, y)
+}
+
+fun isInsideTriangle(triangle: Triangle, point: Point): Boolean {
+    val d1 = sign(point, triangle.A, triangle.B)
+    val d2 = sign(point, triangle.B, triangle.C)
+    val d3 = sign(point, triangle.C, triangle.A)
+    val hasNegative = (d1 < 0)  (d2 < 0)  (d3 < 0)
+    val hasPositive = (d1 > 0)  (d2 > 0)  (d3 > 0)
+    return !(hasNegative && hasPositive)
+}
+
+fun sign(p1: Point, p2: Point, p3: Point): Double {
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
 }
